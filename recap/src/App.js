e5938323c81bc98470abcf75cb159f8c5a3f9ed6
happy149 <hosnearahappybd@gmail.com>
2021-02-24 23:52:57 +0600

    fix

--- recap/src/App.js
+++ recap/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Nayok></Nayok>
-      <Nayok></Nayok>
+      <Nayok name="jashim"></Nayok>
+      <Nayok name="shakib"></Nayok>
+      <Nayok name="Bapparaz"></Nayok>
       <Nayok></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -14,15 +15,18 @@ function App() {
     </div>
   );
 }
-function Nayok(){
+function Nayok(props){
+  
+
  const nayokStyle={
    border: '2px solid red',
    margin:'20px'
    
  }
   
-  return (<div style= {nayokStyle}>
-<h1> Ami khol nayok </h1>
+  return (
+  <div style= {nayokStyle}>
+<h1> Ami khol nayok-{props.name} </h1>
 <h3>i have done 5 movie</h3>
 
   </div>)
